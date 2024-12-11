ed8a136d797a3127df3c40d1a478c4d40d136ad3
saiharshita0705 <boreddysaiharshita0705@gmail.com>
2024-12-11 11:24:12 -0500

    Changes to index.js

--- seeds/index.js
+++ seeds/index.js
@@ -2,8 +2,12 @@ const mongoose = require('mongoose');
 const cities = require('./cities');
 const {places, descriptors} = require('./seedHelpers');
 const Campground = require('../models/campground');
-mongoose.connect('mongodb://localhost:27017/yelp-camp');
 
+require("dotenv").config();
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
+
+mongoose.connect(dbUrl);
+  
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, 'Connection Error'));
 db.once("open", () => {
